c39323d990d63f85d12ecbc18d86ce04c269e37f
laxmikant-parate <laxmikantparate01@gmail.com>
2024-01-01 22:03:48 +0530

    3

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
 import datetime
 
-# Get current UTC time
-utc_now = datetime.datetime.utcnow()
-
-# Convert to India Standard Time (IST)
-india_now = utc_now.astimezone(datetime.timezone(datetime.timedelta(hours=5.5)))
-
-# Print the time
-print(f"Current time in India (IST): {india_now}")
+print(datetime.datetime.now().time())
